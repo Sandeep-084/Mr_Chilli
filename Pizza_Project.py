8e6edae6f584f800803fdf4e4016b18e9a532570
Sandeep-084 <85067856+Sandeep-084@users.noreply.github.com>
2024-10-14 13:56:05 -0500

    Pizza_Project.py

--- Pizza_Project.py
+++ Pizza_Project.py
@@ -1,23 +1,26 @@
 print("Welcome to the Python Pizza Deliveries!!")
-size = input("What size pizza do you want?? S, M, or L : ").upper
-Pepperoni = input("DO you want pepporoni on your pizza? y or N : ").lower
-extra_cheese = input("Do you want extra cheese ? Y or N : ").upper
+size = input("What size pizza do you want? S, M, or L: ").upper()  # Added ()
+Pepperoni = input("Do you want pepperoni on your pizza? y or n: ").lower()  # Added ()
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()  # Added ()
 bill = 0
+
 if size == "S":
-     bill+= 15
+    bill += 15
 elif size == "M":
-     bill = 20
+    bill += 20
 elif size == "L":
-     bill = 25
+    bill += 25
 else:
-     print("Wrong entry")
+    print("Wrong entry for size.")
+    exit()
 
-if pepperoni == "y":
-     if size = " s":
-          bill+= 2
-     else:
-          bill+=3
-if extra_cheese == y:
-     then 
+if Pepperoni == "y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
 
+if extra_cheese == "Y":
+    bill += 1
 
+print(f"Your final bill is ${bill}")
