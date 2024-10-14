6f461273f4304ba3454ab47b73c621730ea39ca8
Sandeep-084 <85067856+Sandeep-084@users.noreply.github.com>
2024-10-14 12:26:36 -0500

    Add files via upload

---
+++ Pizza_Project.py
@@ -0,0 +1,23 @@
+print("Welcome to the Python Pizza Deliveries!!")
+size = input("What size pizza do you want?? S, M, or L : ").upper
+Pepperoni = input("DO you want pepporoni on your pizza? y or N : ").lower
+extra_cheese = input("Do you want extra cheese ? Y or N : ").upper
+bill = 0
+if size == "S":
+     bill+= 15
+elif size == "M":
+     bill = 20
+elif size == "L":
+     bill = 25
+else:
+     print("Wrong entry")
+
+if pepperoni == "y":
+     if size = " s":
+          bill+= 2
+     else:
+          bill+=3
+if extra_cheese == y:
+     then 
+
+
